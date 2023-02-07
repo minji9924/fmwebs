ef80a030c447b35089e6abe6d27e2baaaf00f9ed
Minji <pureb_9924@naver.com>
2023-02-07 21:39:37 +0900

    fm100 테스트 페이지 구현 시작

--- fm100/urls.py
+++ fm100/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('aboutfm100/', views.aboutfm100, name='aboutfm100'),
+    path('testpage/', views.testpage, name='testpage')
 ]
