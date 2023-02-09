1764dabd3e358922f77f14add8ed819e37523062
Minji <pureb_9924@naver.com>
2023-02-09 20:24:00 +0900

    로그인 로그아웃 구현 완

--- fm100/urls.py
+++ fm100/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('aboutfm100/', views.aboutfm100, name='aboutfm100'),
-    path('testpage/', views.testpage, name='testpage')
+    path('testpage/', views.testpage, name='testpage'),
+    path('login/', views.loginpage, name='login'),
 ]
